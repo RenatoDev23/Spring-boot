c86a414ec80c66fe2375a340691d22cc8cc82900
RenatoDev23 <renatorenatex20@gmail.com>
2023-09-14 23:37:47 -0300

    spring boot

---
+++ projetocomspringbootejava/src/test/java/com/projetocomspringbootejava/projetocomspringbootejava/ProjetocomspringbootejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocomspringbootejava.projetocomspringbootejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocomspringbootejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
